de1bce99858b006faab2027f49ac174721055455
rxtATX <rachel.thiim@gmail.com>
2017-03-08 17:46:07 -0600

    console logging cheerio titles correctly

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -9,4 +9,19 @@ router.get("/saved", function(req, res) {
 		res.render("saved", {title: 'News Scraper'});
 });
 
+var request = require("request");
+var cheerio = require("cheerio");
+
+request('https://www.bbc.com/news/world', function (error, response, html) {
+  if (!error && response.statusCode == 200) {
+    var $ = cheerio.load(html);
+	$('span.title-link__title-text').each(function(i, element) {
+		var title = $(this);
+		var a = $(this).parent().parent();
+		console.log(title.text(), a.href);
+
+	})
+  }
+});
+
 module.exports = router;
