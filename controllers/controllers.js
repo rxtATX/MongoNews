e787968689a43c7a3104fc42be541b9cbc5ee4c1
rxtATX <rachel.thiim@gmail.com>
2017-03-09 06:44:30 -0600

    correctly grabs the last index of desired object. no communication to front end.

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,27 +1,61 @@
 var express = require("express");
 var router = express.Router();
 
-router.get("/", function(req, res) {
-		res.render("index", {title: 'News Scraper'});
+router.get("/", function (req, res) {
+	res.render("index", { title: 'News Scraper' });
 });
 
-router.get("/saved", function(req, res) {
-		res.render("saved", {title: 'News Scraper'});
+router.get("/saved", function (req, res) {
+	res.render("saved", { title: 'News Scraper' });
 });
 
 var request = require("request");
 var cheerio = require("cheerio");
 
-request('https://www.bbc.com/news/world', function (error, response, html) {
-  if (!error && response.statusCode == 200) {
-    var $ = cheerio.load(html);
-	$('span.title-link__title-text').each(function(i, element) {
-		var title = $(this);
-		var a = $(this).parent().parent();
-		console.log(title.text(), a.href);
+var objArr = [];
+var metadata = {
+	id: null,
+	title: null,
+	link: null,
+	image: null,
+	story: null
+};
 
-	})
-  }
+request('https://www.bbc.com/news/world', function (error, response, html) {
+	if (!error && response.statusCode == 200) {
+		var $ = cheerio.load(html);
+		$('span.title-link__title-text').each(function (i, element) {
+			var title = $(this).text();
+			var a = $(this).parent().parent();
+			var aRoot = 'https://www.bbc.com';
+			var aPath = a.attr('href');
+			a = aRoot + aPath;
+
+			metadata.id = i;
+			metadata.title = title;
+			metadata.link = a;
+			return i < 19;
+		});
+	}
+	request(metadata.link, function (error, response, html) {
+		if (!error && response.statusCode == 200) {
+			var $ = cheerio.load(html);
+			$('img').each(function (i, element) {
+				if ($(this).hasClass('js-image-replace')) {
+					var image = $(this).attr('src');
+					metadata.image = image;
+				}
+			});
+			$('p').each(function (i, element) {
+				if ($(this).hasClass('story-body__introduction')) {
+					var story = $(this).text();
+					metadata.story = story;
+				}
+			});
+		}
+		console.log(metadata);
+	});
 });
+
 
 module.exports = router;
--- public/assets/javascript/app.js
+++ public/assets/javascript/app.js
@@ -1,7 +1,11 @@
 $(function () { //Document ready
-    $("")
+    // $("")
     $(".navbar-btn").on("click", function() { //Click listener for "Scrape Articles"
         // Get Articles
-        // Display 20
+        $.post("/getArticles").done(function(result){
+            $.each(result, function(index, value) {
+                console.log(value);
+            }); //End each result
+        }); //End get Route
     }); //End Scrape Articles click listener
 }); //End document ready
