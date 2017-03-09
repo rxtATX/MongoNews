1b24fbf4ae2613cd1e07ed5fe11f83dc99f31340
rxtATX <rachel.thiim@gmail.com>
2017-03-09 16:08:18 -0600

    titles are links to articles and button is centered in well. did a little styling

--- public/assets/javascript/app.js
+++ public/assets/javascript/app.js
@@ -1,12 +1,14 @@
 $(function () { //Document ready
     // $("")
     $(".navbar-btn").on("click", function() { //Click listener for "Scrape Articles"
+        $("#articleAppend").empty();
         // Get Articles
         $.post("/getArticles").done(function(result){
             $.each(result, function(index, value) {
                 var title = value.title;
                 var link = value.link;
-                $("#articleAppend").append("<div class='well'><p>" + title + "</p><p>" + link + "</p><div>");
+                var buttons = $('<button type="submit" class="btn btn-success btn-block">Save</button>');
+                $("#articleAppend").append("<div class='well'><p><a href=" + link + " target='_blank'>" + title + "</a><button type='submit' class='btn btn-default pull-right' type='submit'>Save</button></p></div>");
             }); //End each result
         }); //End get Route
     }); //End Scrape Articles click listener
